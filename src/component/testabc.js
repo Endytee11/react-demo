8cfa1e39b2bc559e458d7be538d6cbfe69079bd4
TANND <fvn.tannd@gmail.com>
2021-10-23 21:55:57 +0700

    ok conflict

--- src/component/testabc.js
+++ src/component/testabc.js
@@ -4,5 +4,5 @@ import React, {Fragment} from "react"
     <h1>ko co dau</h1>
     <h1>ko  222</h1>
 <h1>ko co gi dau 3333</h1>
-<h1>tao conflict choi</h1>
+<h1>tao conflict choi 44444444</h1>
 </Fragment>
